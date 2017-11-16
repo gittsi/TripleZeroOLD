a18c076d401900a846472e71c318adea17b6c942
Panagiotis Panagiotopoulos <ppanagiotopoulos@realize.gr>
2017-11-16 11:03:27 +0200

    temp

---
+++ TripleZero/Helper/Caching/ICaching.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace TripleZero.Helper.Caching
+{
+    public interface ICaching<T> : ICaching
+    {
+        T Init();
+    }
+
+    public interface ICaching
+    {
+        string Name();
+        int Minutes();
+    }
+}
