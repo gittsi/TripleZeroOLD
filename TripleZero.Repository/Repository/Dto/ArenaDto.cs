6c8f999263064f34254548b116d0a26fbef1bc51
Panagiotis Panagiotopoulos <ppanagiotopoulos@realize.gr>
2017-12-11 17:35:48 +0200

    bug deletemessage

--- TripleZero.Repository/Repository/Dto/ArenaDto.cs
+++ TripleZero.Repository/Repository/Dto/ArenaDto.cs
@@ -7,7 +7,7 @@ namespace TripleZero.Repository.Dto
     public class ArenaDto
     {
         public int CurrentRank { get; set; }
-        public int AverageRank { get; set; }
+        public double AverageRank { get; set; }
         public List<string> ArenaTeam { get; set; }
     }
 }
